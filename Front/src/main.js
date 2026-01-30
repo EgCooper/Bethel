9c8531c441ce72300fa63d5402814cede6652fd0
VictorPintoMora <victor.pinto@jala.university>
2026-01-30 11:18:53 -0400

    Fixing main error whit url to backend

--- Front/src/main.js
+++ Front/src/main.js
@@ -1,7 +1,7 @@
 import App from './App.svelte';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://bettel-backend.onrender.com'; 
+axios.defaults.baseURL = 'https://bethel-backend-hbst.onrender.com/'; 
 
 const app = new App({
 	target: document.body
